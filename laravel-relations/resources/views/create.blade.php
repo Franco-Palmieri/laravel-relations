cdcbf4b85a66b781fca8f8fc19d3681e573043e7
Ncesecrede <francescopm95@gmail.com>
2021-09-23 16:31:50 +0100

    News

---
+++ laravel-relations/resources/views/create.blade.php
@@ -0,0 +1,5 @@
+@extends('news.base')
+
+@section('content')
+    
+@endsection
--- laravel-relations/routes/web.php
+++ laravel-relations/routes/web.php
@@ -15,3 +15,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'NewsController@index');
 
+Route::resource('news', 'NewsController');
+
